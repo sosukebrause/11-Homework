ca6d72f89732e231f025788510b3df42baf3e4ec
Sosuke Brause <brosuke89@gmail.com>
2020-07-21 03:32:49 -0700

    clientRoutes.js functioning on live server

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 const apiRoutes = require("./routes/api-routes");
-app.use(apiRoutes);
+// app.use(apiRoutes);
 
 const clientRoutes = require("./routes/client-routes");
 app.use(clientRoutes);
